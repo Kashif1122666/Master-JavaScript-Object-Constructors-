cf07326c84e220e2412e689c49cda6c2d18dd12e
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-10-06 01:17:28 -0700

    Add files via upload
    Master JavaScript Object Constructors!

---
+++ script.js
@@ -0,0 +1,42 @@
+
+// function Car(color, model, year) {
+//     this.color = color;
+//     this.model = model;
+//       this.year = year;
+// }
+
+//  address your confusion with names  
+
+// function Car(a, b, c) {
+//     this.color = a;
+//     this.model = b;
+//       this.year = c;
+// }
+
+// create new Objects using constructor function 
+
+// var car1 = new Car("silver","Honda",2014);
+// var car2 = new Car("blue","Toyota",2018);
+// console.table([car1,car2]);
+// console.log(car1);
+// console.log(car2);
+
+// Add methods to constructor 
+
+// function Car(a, b, c) {
+//     this.color = a;
+//     this.model = b;
+//       this.year = c;
+//       this.start = function () {
+//         console.log(this.model + "  has started");
+//     };
+// }
+
+// Testing constructor method 
+
+// var car1 = new Car("black" , "honda",2014);
+// var car2 = new Car("blue" , "Toyota",2019);
+// console.log(car1) ;
+// console.log(car2) ;
+// car1.start();
+// car2.start();
